ed66fa968cfc0788c9faefa3c2fb9c0726389d4c
Shivateja Annaram <shivateja1.annaram@gmail.com>
2025-04-30 11:38:01 +0530

    removed node_modules from git tracking

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const app = require("express");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hi first route");
+});
+
+app.listen(3000, () => {
+  console.log("Server running at localhost 3000");
+});
